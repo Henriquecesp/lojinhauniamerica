a6b79f8f619f017352b29d0d96e0e85470791a97
Henrique Cesp <48498237+Henriquecesp@users.noreply.github.com>
2019-11-07 06:37:44 -0300

    adicao de logica nos produtos do carrinho

--- html/js/script.js
+++ html/js/script.js
@@ -1,6 +1,5 @@
 function showDiv() {
     var cart = document.getElementById('cart');
-    var menu = document.getElementById('menu');
     if (cart.style.display === 'none'){
             cart.style.display = "block";
         }else{  
@@ -11,3 +10,24 @@ function showDiv() {
 function hideDiv(){
     document.getElementById('cart').style.display ="none";
 } 
+
+function delItem1(){
+    var item2 = document.getElementById('item2');
+    if (item2.style.display === 'none'){
+        document.getElementById('none').style.display ="flex"
+    }
+    document.getElementById('item1').style.display = "none"
+}
+
+function delItem2(){
+    var item1 = document.getElementById('item1');
+    if (item1.style.display === 'none'){
+        document.getElementById('none').style.display ="flex"
+    }
+    document.getElementById('item2').style.display = "none"
+}
+
+function addItem1(){
+    document.getElementById('item1').style.display = "flex"
+    document.getElementById('none').style.display ="none"
+}
